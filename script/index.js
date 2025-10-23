286fa1d168e28ee03596b6c94743e95afcb56e75
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-23 20:39:33 +0600

    lesson part

--- script/index.js
+++ script/index.js
@@ -4,6 +4,54 @@ const loadLessons=() =>{
     .then((json) => displayLesson(json.data))
 
 };
+const loadLevelWord = (id) => {
+    console.log("Level ID:", id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("Level Data:", data);
+            // ✅ এখন এখানে data.data.words পাঠাও displayLevelWord() এ
+            displayLevelWord(data.data);
+        })
+        .catch((error) => console.error(error));
+};
+
+const displayLevelWord=(words) =>{
+       const wordContainer = document.getElementById("word-container")
+       wordContainer.innerHTML = "";
+
+       words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+         card.innerHTML = `<div class="bg-white  rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">Eager</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="text-xl font-medium font-bangla">
+             "আগ্রহী / ইগার"
+        </div>
+        <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#188dfa80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn  bg-[#1A91FF10]  hover:bg-[#188dfa80]"><i class="fa-solid fa-volume-high"></i>
+            </button>
+        </div>
+
+    </div>`;
+        
+    // 🟢 নতুন এই লাইন
+    wordContainer.append(card);
+       });
+
+};
+
+
+
+
+
+
+
+
+
 const displayLesson = (lessons) => {
     // 1.get the container & empty
     const levelContainer = document.getElementById("level-container");   
@@ -14,7 +62,7 @@ const displayLesson = (lessons) => {
          console.log(lessons);
          const btnDiv = document.createElement("div")
          btnDiv.innerHTML = `
-         <button class="btn btn-outline btn-primary">
+         <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
          <i class=" fa-solid fa-circle-question"></i>LESSON - ${lesson.level_no}
          </button>
 
@@ -35,4 +83,27 @@ const displayLesson = (lessons) => {
 
      
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 loadLessons();
